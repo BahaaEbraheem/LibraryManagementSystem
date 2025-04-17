8abda34f9a15a8ae869ba9696841adb2d356c6aa
bahaa ebraheem <Bahaa.ebraheem812@outlook.com>
2025-04-17 13:53:16 +0300

    commit delete and update book

--- lms_app/views.py
+++ lms_app/views.py
@@ -1,7 +1,7 @@
 
 
 
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect, render
 from .forms import bookForm,CategoryForm
 from .models import Book, Category
 
@@ -37,8 +37,25 @@ def books(request):
      
      return render(request,'pages/books.html',context)
 
-def update(request):
-    return render(request,'pages/update.html')
+def update(request, id):
+    book = get_object_or_404(Book, id=id)
 
-def delete(request):
-    return render(request,'pages/delete.html') ; 
+    if request.method == 'POST':
+        edit_book = bookForm(request.POST, request.FILES, instance=book)
+        if edit_book.is_valid():
+            edit_book.save()
+        else:
+            print("❌ أخطاء في نموذج الكتاب:", edit_book.errors)
+    else:
+        edit_book = bookForm(instance=book)
+
+    return render(request, 'pages/update.html', {'form': edit_book, 'book': book})
+
+def delete(request, id):
+    book = get_object_or_404(Book, id=id)
+
+    if request.method == 'POST':
+        book.delete()
+        return redirect('/')  # or redirect to your book list page
+
+    return render(request, 'pages/delete.html', {'book': book})
